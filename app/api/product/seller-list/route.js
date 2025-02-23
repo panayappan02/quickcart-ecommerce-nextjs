aaa2ce1d197f6963e0866cfa45ddeb947a5f1f60
panayappan02 <panayappan02@gmail.com>
2025-02-23 22:56:11 +0530

    Added: Order inngest configuration

---
+++ app/api/product/seller-list/route.js
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import { getAuth } from "@clerk/nextjs/server";
+
+import authSeller from "@/lib/authSeller";
+import Product from "@/models/Product";
+import connectDB from "@/config/db";
+
+export async function GET(request) {
+  try {
+    const userId = getAuth(request);
+
+    const isSeller = await authSeller(userId);
+
+    if (!isSeller) {
+      return NextResponse.json({
+        success: false,
+        message: "not authorized",
+      });
+    }
+
+    await connectDB();
+
+    const products = await Product.find({});
+
+    return NextResponse.json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
